0823c71cee8237936d1705f2218b7e6cd7728457
ImJulyBorn <106574769+ImJulyBorn@users.noreply.github.com>
2022-06-17 20:54:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('i-look-great-in-mustaches.png')
+}
